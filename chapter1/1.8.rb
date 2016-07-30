9c7c103e3aae8cce14885d5ede7b13a3381a43cd
suman <bappi.5476@gmail.com>
2016-07-30 23:30:18 +0530

    8 example

--- chapter1/1.8.rb
+++ chapter1/1.8.rb
@@ -1,3 +1,11 @@
 # Assume you have a method isSubstring which checks if one word is a substring of another 
 # Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 
 # using only one call to isSubstring (i e , “waterbottle” is a rotation of “erbottlewat”)
+
+def rotation(s1,s2)
+  return false if s1.length != s2.length
+  s1s1 = s1+s1
+  isSubstring(s1s1,s2)
+end
+
+
